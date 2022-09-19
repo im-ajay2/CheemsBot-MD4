13cf956c46d9649df696dff9444cf5f7806285be
im-ajay2 <113838326+im-ajay2@users.noreply.github.com>
2022-09-19 22:59:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -25,7 +25,7 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //Api Website\\
-global.APIs = {
+global.APIs =
 	zenz: 'https://zenzapis.xyz',
 }
 
@@ -54,8 +54,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['917059114934'] //ur owner number
+global.ownername = "ajay" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
