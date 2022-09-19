cab779284c847f31235be044759fd46623aaea3a
im-ajay2 <113838326+im-ajay2@users.noreply.github.com>
2022-09-19 18:01:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -55,14 +55,14 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
 global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.ownername = "ajay" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.location = "West Bengal India" //ur location
 
 //bot bomdy 
 global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
+global.ownertag = '7059114934' //ur tag number
 global.botname = 'Cheems Bot MD V4' //ur bot name
 global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
